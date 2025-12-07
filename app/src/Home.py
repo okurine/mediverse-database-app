cf98bd51d6a53885781afcdaeea8946d439be05a
BAlwis <alwis.b@northeastern.edu>
2025-12-07 01:35:39 +0000

    sample data added

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,6 +1,6 @@
 ##################################################
 # This is the main/entry-point file for the 
-# sample application for your project
+# sample application for Midiverse project
 ##################################################
 
 # Set up basic logging infrastructure
@@ -44,7 +44,7 @@ if st.button('Act as John Joe, an Patient ',
             use_container_width=True):
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'patient'
-    st.session_state['first_name'] = 'Mohammad'
+    st.session_state['first_name'] = 'John'
     st.switch_page('pages/10_patient_Home.py')
 
 if st.button("Act as Maya Patel, a Clinician", 
